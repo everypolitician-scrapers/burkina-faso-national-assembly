616eaf808acc9de01ee2949369322bfbd404ffc0
Struan Donald <struan@exo.org.uk>
2016-03-21 16:43:22 +0000

    add the 7th term

--- scraper.rb
+++ scraper.rb
@@ -11,23 +11,36 @@ require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
+  content = ''
+  begin
+    content = open(url).read
+  rescue OpenURI::HTTPError => e
+    # ignore errors because some of the pages return a 500
+    # code even though the page works.
+    content = e.io
+  end
+  Nokogiri::HTML(content)
 end
 
-def scrape_list(url)
-  noko = noko_for(url)
+def scrape_list(url, term)
+  noko = noko_for(url) rescue nil
+  if noko.nil?
+      return
+  end
+  puts '%s - %s' % [url, term]
   noko.css('#Contenu a[href*="page=depute"]/@href').map(&:text).each do |href|
-    scrape_mp(URI.join url, href)
+    scrape_mp(URI.join(url, href), term)
   end
 end
 
 def gender_from(name)
   return 'male' if name.start_with? 'Mr'
+  return 'male' if name.start_with? 'M.'
   return 'female' if name.start_with? 'Mme'
   raise "Unknown gender for #{name}"
 end
 
-def scrape_mp(url)
+def scrape_mp(url, term)
   noko = noko_for(url)
   box = noko.css('#Contenu table.sancel')
   party_info = box.xpath('.//td[contains(.,"Parti politique")]/following-sibling::td').text
@@ -41,18 +54,29 @@ def scrape_mp(url)
     area: box.xpath('.//td[contains(.,"Liste provinciale")]/following-sibling::td[1]').text,
     email: box.css('a[href*="mailto"]/@href').text.sub('mailto:',''),
     image: box.css('img[src*="photos"]/@src').text,
-    term: 2012,
+    term: term,
   }
   data[:gender] = gender_from(data[:name])
   ScraperWiki.save_sqlite([:name, :term], data)
 end
 
-term = {
-  id: 2012,
-  name: '2012–2015',
-  start_date: '2012-12-02',
-}
-ScraperWiki.save_sqlite([:id], term, 'terms')
+terms = [
+    {
+      id: 2012,
+      name: '2012–2015',
+      start_date: '2012-12-02',
+      end_date: '2016-01-10',
+      source: 'http://www.assembleenationale.bf/spip.php?article55',
+    },
+    {
+      id: 2016,
+      name: '2016–2019',
+      start_date: '2016-01-02',
+      source: 'http://www.assembleenationale.bf/Deputes-de-la-VIIeme-legislature',
+    },
+]
 
-
-scrape_list('http://www.assembleenationale.bf/spip.php?article55')
+terms.each do |term|
+    ScraperWiki.save_sqlite([:id], term, 'terms')
+    scrape_list(term[:source], term[:id])
+end
