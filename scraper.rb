0f1164e07569d3d13b15e4ede026a0765fa5fd0c
Tony Bowden <tony@tmtm.com>
2017-01-08 18:40:33 +0000

    Clean out database on each run

--- scraper.rb
+++ scraper.rb
@@ -53,4 +53,5 @@ def scrape_mp(url)
   ScraperWiki.save_sqlite(%i(id term), data)
 end
 
+ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
 scrape_list('http://www.assembleenationale.bf/Deputes-de-la-VIIeme-legislature')
